4cbee30d7d1a7c26c086e812c3c92d03c4a15e73
theo da-silva <theo.da-silva@epitech.eu>
2016-05-23 15:23:21 +0200

    Ajout du header

--- lib/my/my_strcmp.c
+++ lib/my/my_strcmp.c
@@ -1,3 +1,13 @@
+/*
+** my_strcmp.c for my_strcmp in /home/da-sil_t/rendu/Systeme_Unix/42sh/PSU_2015_42sh
+** 
+** Made by theo da-silva
+** Login   <da-sil_t@epitech.net>
+** 
+** Started on  Mon May 23 15:22:26 2016 theo da-silva
+** Last update Mon May 23 15:22:27 2016 theo da-silva
+*/
+
 int	my_strncmp(char *s1, char *s2)
 {
   int   i;
